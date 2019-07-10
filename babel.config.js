67a1b0c842c287805539c1ca0e2e7e1da3cfbaf7
Sasha <aleksandr.gulchenko@gmail.com>
2019-07-10 14:26:51 +0300

    asad

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function(api) {
   const presets = [ '@babel/preset-env', '@babel/preset-react' ]
   const plugins = [
     '@babel/proposal-class-properties',
+    'react-require',
   ]
 
   return {
